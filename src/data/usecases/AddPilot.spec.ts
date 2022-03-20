5813768bd2323b80fcbbfd99d2b53838169e5fcc
VictorAugDB <vdbernardes@gmail.com>
2022-03-20 15:04:24 -0300

    test: tests CheckShipAlreadyHasOwner

--- src/data/usecases/AddPilot.spec.ts
+++ src/data/usecases/AddPilot.spec.ts
@@ -3,6 +3,7 @@ import {
   ICheckShipAlreadyHasOwner,
   ICheckShipAlreadyHasOwnerInput,
 } from '@/data/contracts/repositories/pilots/CheckShipAlreadyHasOwner'
+import { ICreatePilot } from '@/data/contracts/repositories/pilots/CreatePilot'
 import { IGetPilot } from '@/data/contracts/repositories/pilots/GetPilot'
 import { IGetShip } from '@/data/contracts/repositories/ships/GetShip'
 import { AddPilotUseCase } from '@/data/usecases/AddPillot'
@@ -17,6 +18,7 @@ type ISutTypes = {
   getPilotRepositoryStub: IGetPilot
   getShipRepositoryStub: IGetShip
   checkShipAlreadyHasOwnerRepositoryStub: ICheckShipAlreadyHasOwner
+  createPilotRepositoryStub: ICreatePilot
 }
 
 const makeFakeRequest = (): IAddPilotInput => ({
@@ -59,15 +61,28 @@ const makeCheckShipAlreadyHasOwnerRepositoryStub =
     return new CheckShipAlreadyHasOwnerRepositoryUseCaseStub()
   }
 
+const makeCreatePilotRepositoryStub = (): ICreatePilot => {
+  class CreatePilotRepositoryUseCaseStub implements ICreatePilot {
+    async create(data: IPilot): Promise<IPilot> {
+      return mockFakePilot()
+    }
+  }
+
+  return new CreatePilotRepositoryUseCaseStub()
+}
+
 const makeSut = (): ISutTypes => {
   const getPilotRepositoryStub = makeGetPilotRepositoryStub()
   const getShipRepositoryStub = makeGetShipRepositoryStub()
   const checkShipAlreadyHasOwnerRepositoryStub =
     makeCheckShipAlreadyHasOwnerRepositoryStub()
+  const createPilotRepositoryStub = makeCreatePilotRepositoryStub()
+
   const sut = new AddPilotUseCase(
     getPilotRepositoryStub,
     getShipRepositoryStub,
     checkShipAlreadyHasOwnerRepositoryStub,
+    createPilotRepositoryStub,
   )
 
   return {
@@ -75,6 +90,7 @@ const makeSut = (): ISutTypes => {
     getPilotRepositoryStub,
     getShipRepositoryStub,
     checkShipAlreadyHasOwnerRepositoryStub,
+    createPilotRepositoryStub,
   }
 }
 
@@ -122,6 +138,36 @@ describe('AddPilot', () => {
       const fakeRequest = makeFakeRequest()
       jest
         .spyOn(getShipRepositoryStub, 'getById')
+        .mockRejectedValueOnce(new Error())
+      const promise = sut.execute(fakeRequest)
+
+      await expect(promise).rejects.toThrowError()
+    })
+  })
+
+  describe('CheckShipAlreadyHasOwner', () => {
+    test('Should call CheckShipAlreadyHasOwner with correct values', async () => {
+      const { sut, checkShipAlreadyHasOwnerRepositoryStub } = makeSut()
+      const fakeRequest = makeFakeRequest()
+      const getShipRepoSpy = jest.spyOn(
+        checkShipAlreadyHasOwnerRepositoryStub,
+        'checkShipAlreadyHasOwner',
+      )
+      await sut.execute(fakeRequest)
+
+      expect(getShipRepoSpy).toHaveBeenCalledWith({
+        shipId: fakeRequest.shipId,
+      })
+    })
+
+    test('Should throw if CheckShipAlreadyHasOwner throws', async () => {
+      const { sut, checkShipAlreadyHasOwnerRepositoryStub } = makeSut()
+      const fakeRequest = makeFakeRequest()
+      jest
+        .spyOn(
+          checkShipAlreadyHasOwnerRepositoryStub,
+          'checkShipAlreadyHasOwner',
+        )
         .mockRejectedValueOnce(new Error())
       const promise = sut.execute(fakeRequest)
 
