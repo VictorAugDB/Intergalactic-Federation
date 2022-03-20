7e8511ae3b94f1c78ab3329f9d3e7c33c44a424e
VictorAugDB <vdbernardes@gmail.com>
2022-03-20 11:11:26 -0300

    feat: ensure calls validate with correct values, return error if validation validate returns an error, and throws if validate throws

---
+++ src/application/controllers/AddPillot.ts
@@ -0,0 +1,27 @@
+import { IAddPilotDTO } from '@/application/dtos/AddPilot'
+import {
+  badRequest,
+  serverError,
+  success,
+} from '@/application/helpers/HttpHelper'
+import {
+  IController,
+  IRequest,
+  IResponse,
+} from '@/application/protocols/Controller'
+import { IValidation } from '@/application/protocols/Validation'
+
+export class AddPilot implements IController {
+  constructor(private readonly validation: IValidation) {}
+
+  async handle(req: IRequest<IAddPilotDTO>): Promise<IResponse<any>> {
+    try {
+      const error = this.validation.validate(req.body)
+      if (error) return badRequest(error)
+
+      return success('success')
+    } catch (err) {
+      return serverError(err)
+    }
+  }
+}
---
+++ src/application/dtos/AddPilot.ts
@@ -0,0 +1,8 @@
+export type IAddPilotDTO = {
+  certificationDocument: string
+  shipId: string
+  name: string
+  age: number
+  credits: number
+  locationPlanet: string
+}
