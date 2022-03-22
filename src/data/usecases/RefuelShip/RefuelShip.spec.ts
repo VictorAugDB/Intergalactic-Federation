b310fa90feb6fd6eec57636687ae63ec4f6fdba3
VictorAugDB <vdbernardes@gmail.com>
2022-03-21 21:09:10 -0300

    test: add GetPilotRepository with correct values

--- src/data/usecases/RefuelShip/RefuelShip.spec.ts
+++ src/data/usecases/RefuelShip/RefuelShip.spec.ts
@@ -1,9 +1,12 @@
+import { IGetPilot } from '@/data/contracts/repositories/pilots/GetPilot'
 import { IGetShip } from '@/data/contracts/repositories/ships/GetShip'
 import { IUpdateShip } from '@/data/contracts/repositories/ships/UpdateShip'
+import { makeGetPilotRepositoryStub } from '@/data/mocks/stubs/makeGetPilotRepositoryStub'
 import { makeGetShipRepositoryStub } from '@/data/mocks/stubs/makeGetShipRepositoryStub'
 import { makeUpdateShipRepositoryStub } from '@/data/mocks/stubs/makeUpdateShipRepositoryStub'
 import { RefuelShipUseCase } from '@/data/usecases/RefuelShip/RefuelShip'
 import { IRefuelShipInput } from '@/domain/usecases/RefuelShip'
+import { mockFakePilot } from '@/shared/mocks/fakePilot'
 import { mockFakeShip } from '@/shared/mocks/fakeShip'
 
 jest.mock('uuid', () => ({
@@ -14,32 +17,63 @@ type ISutTypes = {
   sut: RefuelShipUseCase
   getShipRepositoryStub: IGetShip
   updateShipRepositoryStub: IUpdateShip
+  getPilotRepositoryStub: IGetPilot
 }
 
 const makeFakeRequest = (): IRefuelShipInput => ({
-  quantity: 10,
-  shipId: 'any_id',
+  certificationDocument: 'any_document',
+  amountOfFuel: 10,
 })
 
 const makeSut = (): ISutTypes => {
+  const getPilotRepositoryStub = makeGetPilotRepositoryStub(mockFakePilot())
   const getShipRepositoryStub = makeGetShipRepositoryStub()
   const updateShipRepositoryStub = makeUpdateShipRepositoryStub({
-    fuelCapacity: makeFakeRequest().quantity,
+    fuelCapacity: makeFakeRequest().amountOfFuel,
   })
 
   const sut = new RefuelShipUseCase(
+    getPilotRepositoryStub,
     getShipRepositoryStub,
     updateShipRepositoryStub,
   )
 
   return {
     sut,
+    getPilotRepositoryStub,
     getShipRepositoryStub,
     updateShipRepositoryStub,
   }
 }
 
 describe('PublishContractUseCase', () => {
+  describe('GetPilotRepository', () => {
+    test('Should call GetPilotRepository with correct values', async () => {
+      const { sut, getPilotRepositoryStub } = makeSut()
+      const fakeRequest = makeFakeRequest()
+      const getPilotRepoSpy = jest.spyOn(
+        getPilotRepositoryStub,
+        'getByDocument',
+      )
+      await sut.execute(fakeRequest)
+
+      expect(getPilotRepoSpy).toHaveBeenCalledWith(
+        fakeRequest.certificationDocument,
+      )
+    })
+
+    test('Should throw if GetPilotRepository throws', async () => {
+      const { sut, getPilotRepositoryStub } = makeSut()
+      const fakeRequest = makeFakeRequest()
+      jest
+        .spyOn(getPilotRepositoryStub, 'getByDocument')
+        .mockRejectedValueOnce(new Error())
+      const promise = sut.execute(fakeRequest)
+
+      await expect(promise).rejects.toThrowError()
+    })
+  })
+
   describe('GetShipRepository', () => {
     test('Should call GetShipRepository with correct values', async () => {
       const { sut, getShipRepositoryStub } = makeSut()
@@ -71,7 +105,7 @@ describe('PublishContractUseCase', () => {
 
       expect(updateShipRepoSpy).toHaveBeenCalledWith({
         id: 'any_id',
-        fuelLevel: mockFakeShip().fuelLevel + fakeRequest.quantity,
+        fuelLevel: mockFakeShip().fuelLevel + fakeRequest.amountOfFuel,
       })
     })
 
@@ -94,7 +128,7 @@ describe('PublishContractUseCase', () => {
     const result = await sut.execute(fakeRequest)
 
     expect(result).toEqual({
-      fuelLevel: mockFakeShip().fuelLevel + fakeRequest.quantity,
+      fuelLevel: mockFakeShip().fuelLevel + fakeRequest.amountOfFuel,
     })
   })
 })
